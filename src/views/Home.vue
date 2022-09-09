585a7b02230aadddf97ed3bbbe6605ad8d707936
MikeleP <mikelepaladin47@gmail.com>
2022-09-09 21:13:22 +0200

    manje korekcije

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,55 @@
 <template>
-  <v-app>
-    <v-container fill-height>
-      <v-col>
-        <v-row class="pa-3 align-center justify-center">
-          <v-btn
-            rounded="lg"
-            color="green lighten-1"
-            class="mr-7 pa-6 elevation-5"
-            width="310px"
-            ><router-link
-              style="text-decoration: none; color: white"
-              to="/podaci"
-            >
-              <v-icon color="purple">mdi-file-document</v-icon>
-              Podaci o govedima
-            </router-link>
-          </v-btn>
-        </v-row>
-        <v-row class="pa-3 align-center justify-center">
-          <v-btn
-            rounded="lg"
-            color="green lighten-1"
-            class="mr-7 pa-6 elevation-5"
-            width="310px"
-            ><router-link
-              style="text-decoration: none; color: white"
-              to="/dolazak"
-            >
-              <v-icon color="purple">mdi-emoticon-cool</v-icon>
-              Dolazak na gospodarstvo
-            </router-link>
-          </v-btn>
-        </v-row>
-        <v-row class="pa-3 align-center justify-center">
-          <v-btn
-            rounded="lg"
-            color="green lighten-1"
-            class="mr-7 pa-6 elevation-5"
-            width="310px"
-            ><router-link
-              style="text-decoration: none; color: white"
-              to="/odlazak"
-            >
-              <v-icon color="purple">mdi-emoticon-sad</v-icon>
-              Odlazak s gospodarstva
-            </router-link>
-          </v-btn>
-        </v-row>
-      </v-col>
-    </v-container>
-  </v-app>
+  <div>
+    <v-app>
+      <v-container fill-height>
+        <v-col class="green lighten-3">
+          <v-row class="pa-3 align-center justify-center">
+            <v-btn
+              rounded
+              color="green lighten-1"
+              class="mr-7 pa-6 elevation-5"
+              width="310px"
+              ><router-link
+                style="text-decoration: none; color: white"
+                to="/podaci"
+              >
+                <v-icon color="purple">mdi-file-document</v-icon>
+                Podaci o govedima
+              </router-link>
+            </v-btn>
+          </v-row>
+          <v-row class="pa-3 align-center justify-center">
+            <v-btn
+              rounded
+              color="green lighten-1"
+              class="mr-7 pa-6 elevation-5"
+              width="310px"
+              ><router-link
+                style="text-decoration: none; color: white"
+                to="/dolazak"
+              >
+                <v-icon color="purple">mdi-emoticon-cool</v-icon>
+                Dolazak na gospodarstvo
+              </router-link>
+            </v-btn>
+          </v-row>
+          <v-row class="pa-3 align-center justify-center">
+            <v-btn
+              rounded
+              color="green lighten-1"
+              class="mr-7 pa-6 elevation-5"
+              width="310px"
+              ><router-link
+                style="text-decoration: none; color: white"
+                to="/odlazak"
+              >
+                <v-icon color="purple">mdi-emoticon-sad</v-icon>
+                Odlazak s gospodarstva
+              </router-link>
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-container>
+    </v-app>
+  </div>
 </template>
